3e0e93968c4bf4168ffcf98b44b54f971465f37e
yejinyoun <yejinyoun@icloud.com>
2023-02-07 14:43:47 +0100

    basic setup for big fat strings

---
+++ script.js
@@ -0,0 +1,12 @@
+/* If input is a first name: Make the first character in a name uppercase, and the rest lowercase
+If input is a full name: Find the first name
+If input is a full name: Find the length of the first name
+If input is a full name: Find the middle name start and end position, and the middle name itself in a full name string
+If input is a filename: Check if a filename is .png or .jpg
+If input is a password: Hide a password with the correct number of *s
+With any input: Make the third character uppercase
+With any input: Make a character uppercase, if it follows a space or a hyphen
+note: Start with handling just the first space, and/or the first hyphen. Plan on how you would make it repeat for multiple occurences, even if you can’t code it succesfully.
+When you click the button, it processes whatever is in the inputbox, and writes the result in the output box */
+
+"use strict";
